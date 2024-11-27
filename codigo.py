8306ba656b321eda29ed4d98acec1a483c14f043
Soy Joaco <joaquinaparicio02@gmail.com>
2024-11-27 13:18:26 -0300

    actualizando

--- codigo.py
+++ codigo.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 nombre = "Pepe"
+=======
+nombre = "Joaco"
+>>>>>>> b3cfc64cfbb3abe74e6b96bf7e4c9c6b5794e84e
 apellido = "Aparicio"
 
 saludo = "Hola"+ nombre + " " + apellido + " ¿Como estas?"
